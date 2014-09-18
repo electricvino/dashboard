65295c52bd0afbd2ba4e49fc0175290f95d16f5f
Jon Vinson <jvinson@spp.org>
2014-09-18 14:02:18 -0500

    initial checkin of a few files with no styling to test git.

---
+++ app.js
@@ -0,0 +1,18 @@
+(function () {
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl:'partials/home.html'
+            })
+            .when("/about", {
+                templateUrl:'partials/about.html'
+            })
+            .when("/resume", {
+				templateUrl:'partials/resume.html'
+			})
+			.otherwise({
+                redirectTo:'/'
+            });
+})();
